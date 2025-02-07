594b22feda8cba498907db91ec6d636dbaddac88
Vishnu <Vishnuxd21@gmail.com>
2025-02-07 17:01:09 -0600

    few changes

--- Gengo/src/server.js
+++ Gengo/src/server.js
@@ -1,14 +1,15 @@
-// filepath: /c:/Users/vishn/Gengo/Gengo/src/server.js
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 const { handleSignaling } = require('./signaling');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, '../public')));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
