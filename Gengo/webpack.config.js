35ccbc355c1018753a482acd2ecb39862a2dd335
Vishnu <Vishnuxd21@gmail.com>
2025-02-08 20:15:07 -0600

    ssda

--- Gengo/webpack.config.js
+++ Gengo/webpack.config.js
@@ -10,7 +10,10 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
+        filename: 'scripts/[name].[contenthash].js',
+        clean: {
+            keep: /assets\// // Preserve assets directory
+        },
         publicPath: '/'
     },
     devServer: {
@@ -43,10 +46,17 @@ module.exports = {
                 use: ['style-loader', 'css-loader']
             },
             {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'assets/[name][ext]'
+                    filename: 'assets/images/[name].[hash][ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name].[hash][ext]'
                 }
             }
         ]
@@ -79,99 +89,15 @@ module.exports = {
                 {
                     from: 'public/styles2.css',
                     to: 'styles2.css'
-                }
-            ]
-        })
-    ],
-    resolve: {
-        extensions: ['.js'],
-        fallback: {
-            "buffer": require.resolve("buffer/")
-        }
-    }
-};
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-module.exports = {
-    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-    entry: {
-        main: './public/scripts/main.js',
-        'language-animation': './public/scripts/language-animation.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        publicPath: '/'
-    },
-    devServer: {
-        port: 9000,
-        hot: true,
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        proxy: {
-            '/socket.io': {
-                target: 'http://localhost:3000',
-                ws: true
-            }
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
+                },
+                {
+                    from: 'public/assets',
+                    to: 'assets',
+                    noErrorOnMissing: true,
+                    globOptions: {
+                        ignore: ['**/.DS_Store']
                     }
                 }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/main.html',
-            filename: 'main.html',
-            chunks: ['main'],
-            inject: 'body'
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-            chunks: ['language-animation'],
-            inject: 'body'
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/about.html',
-            filename: 'about.html',
-            chunks: ['language-animation'],
-            inject: 'body'
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: 'public/styles.css',
-                    to: 'styles.css'
-                },
-                {
-                    from: 'public/styles2.css',
-                    to: 'styles2.css'
-                }
             ]
         })
     ],
@@ -180,5 +106,12 @@ module.exports = {
         fallback: {
             "buffer": require.resolve("buffer/")
         }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            name: false
+        },
+        runtimeChunk: 'single'
     }
 };
